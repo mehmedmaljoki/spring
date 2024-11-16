c7d07f08055d443e422851f011a5d34d7e89ae4c
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2024-11-16 07:13:06 +0100

    add Quicknote and SecurityConfig

---
+++ testing-spring-boot-with-mockmvc/src/main/java/com/mehmedmaljoki/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.mehmedmaljoki;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(
+                        authorize ->
+                                authorize
+                                        .requestMatchers(HttpMethod.GET, "/dashboard")
+                                        .permitAll()
+                                        .requestMatchers(HttpMethod.GET, "/api/tasks/**")
+                                        .authenticated()
+                                        .requestMatchers("/api/users/**")
+                                        .permitAll()
+                                        .requestMatchers("/**")
+                                        .authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
---
+++ testing-spring-boot-with-mockmvc/src/test/java/com/mehmedmaljoki/SecurityConfigTest.java
@@ -0,0 +1,7 @@
+package com.mehmedmaljoki;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SecurityConfigTest {
+
+}
